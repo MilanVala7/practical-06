be4d6ec50e891c22ec224406ee6b2a75eef00383
Milan Vala <valamilan2005@gmail.com>
2026-03-20 18:16:22 +0530

    created event-based processbusinesslogic with processCompleted event

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Events;
+
+internal class Program
+{
+    //summary:
+    //purpose: Prints “Method Invoked” and executes when process is completed.
+    //return type: void
+    static void bl_ProcessCompleted()
+    {
+        Console.WriteLine("Method Invoked");
+    }
+
+    static void Main()
+    {
+        ProcessBusinessLogic b1 = new ProcessBusinessLogic();
+
+        b1.ProcessCompleted += bl_ProcessCompleted;
+
+        b1.StartProcess();
+    }
+}
